e366b77b704762812ea225136b6747670b14b36f
marryblossom <408267415@qq.com>
2015-08-01 17:00:32 +0800

    finished exercise

--- src/com/twu/biblioteca/ExercisesApp.java
+++ src/com/twu/biblioteca/ExercisesApp.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExercisesApp {
     public String horizontal(int n){
         String s = "";
@@ -27,10 +30,126 @@ public class ExercisesApp {
 
         return s;
     }
-    public static void main(String[] args){
-        ExercisesApp a = new ExercisesApp();
-        System.out.println(a.horizontal(3));
-        System.out.println(a.triangle(3));
+
+public String[] isoscelesTriangle(int n){
+    String[] s = new String[n];
+    for (int i = 0; i < n; i++) {
+        s[i] = "";
+        for (int j=0;j<(n-i)-1;j++){
+            s[i] += " ";
+        }
+        for (int j=0;j<2*i+1;j++){
+            s[i] += "*";
+        }
     }
+    return s;
+}
+    public String[] diamond(int n){
+        String[] s = new String[2*n-1];
+        for (int i = 0; i < n; i++) {
+            s[i] = "";
+            for (int j=0;j<(n-i)-1;j++){
+                s[i] += " ";
+            }
+            for (int j=0;j<2*i+1;j++){
+                s[i] += "*";
+            }
+        }
+//        for (int i = n; i < 2*n-1; i++) {
+//            s[i] = "";
+//            for (int j=1;j< (i-n/2-1);j++){
+//                s[i] += " ";
+//            }
+//            for (int j=0;j<n-i/2;j++){
+//                s[i] += "*";
+//            }
+//        }
+        for (int i = n; i < 2*n-1; i++) {
+            s[i] = s[i-n];
+        }
+
+        return s;
+    }
+    public String[] diamondWithName(int n){
+        String[] s = new String[2*n-1];
+        for (int i = 0; i < n-1; i++) {
+            s[i] = "";
+            for (int j=0;j<(n-i)-1;j++){
+                s[i] += " ";
+            }
+            for (int j=0;j<2*i+1;j++){
+                s[i] += "*";
+            }
+        }
+        s[n-1] = "Bill";
+        for (int i = n; i < 2*n-1; i++) {
+            s[i] = "";
+            for (int j=1;j< (i-n/2-1);j++){
+                s[i] += " ";
+            }
+            for (int j=0;j<n-i/2;j++){
+                s[i] += "*";
+            }
+        }
+        return s;
+    }
+    public String[] fizzBuzz(int n){
+        String s[] = new String[n];
+        for (int i = 0; i < n ; i++) {
+            if ((i+1)%3 == 0 && (i+1)%5 == 0){
+                s[i] = "FizzBuzz";
+            } else  if ((i+1)%3 == 0){
+                s[i] = "Fizz";
+            }else  if ((i+1)%5 == 0){
+                s[i] = "Buzz";
+            }else {
+                s[i] = i+1+"";
+            }
+        }
+        return s;
+    }
+
+    public List<Integer> primeFactors(int number) {
+        List<Integer> factorsList = new ArrayList<Integer>();
+        int temp = number;
+        for (int i = 2; i <= number; i++) {
+            if (!isPrime(i)) {
+                continue;
+            }
+            while (true) {
+                if (temp % i == 0) {
+                    temp = temp / i;
+                    factorsList.add(i);
+                } else {
+                    break;
+                }
+            }
+        }
+        return factorsList;
+    }
+
+    private boolean isPrime(int n) {
+        int i;
+        for (i = 2; i <= n; i++) {
+            if (n%i == 0) {
+                break;
+            }
+        }
+        if (i >= n) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+//public static void main(String[] args){
+//    ExercisesApp app = new ExercisesApp();
+//    String[] s = app.fizzBuzz(15);
+//    for (int i = 0; i <s.length ; i++) {
+//        System.out.println(s[i]);
+//    }
+//
+//}
 
 }
--- test/com/twu/biblioteca/ExercisesAppTest.java
+++ test/com/twu/biblioteca/ExercisesAppTest.java
@@ -32,4 +32,60 @@ public class ExercisesAppTest {
     public void testTriangle() throws Exception {
         assertEquals("*\n" + "**\n" + "***\n", exercisesApp.triangle(3));
     }
+    @Test
+    public void testIsoscelesTriangle() throws Exception {
+        String[] s = new String[3];
+        s[0] = "  *";
+        s[1] = " ***";
+        s[2] = "*****";
+        for(int i=0;i<s.length;i++){
+            assertEquals(s[0], exercisesApp.isoscelesTriangle(3)[0]);
+            assertEquals(s[1], exercisesApp.isoscelesTriangle(3)[1]);
+            assertEquals(s[2], exercisesApp.isoscelesTriangle(3)[2]);
+        }
+
+    }
+    @Test
+    public void testFizzBuzz() throws Exception {
+        String[] s = new String[20];
+        s[0] = "1";
+        s[1] = "2";//
+        s[2] = "Fizz";
+        s[3] = "4";
+        s[4] = "Buzz";
+        s[5] = "Fizz";
+        s[6] = "7";
+        s[7] = "8";
+        s[8] = "Fizz";
+        s[9] = "Buzz";
+        s[10] = "11";
+        s[11] = "Fizz";
+        s[12] = "13";
+        s[13] = "14";
+        s[14] = "FizzBuzz";
+        for(int i=0;i<s.length;i++){
+            assertEquals(s[0], exercisesApp.fizzBuzz(15)[0]);
+            assertEquals(s[1], exercisesApp.fizzBuzz(15)[1]);
+            assertEquals(s[2], exercisesApp.fizzBuzz(15)[2]);
+            assertEquals(s[3], exercisesApp.fizzBuzz(15)[3]);
+            assertEquals(s[4], exercisesApp.fizzBuzz(15)[4]);
+            assertEquals(s[5], exercisesApp.fizzBuzz(15)[5]);
+            assertEquals(s[6], exercisesApp.fizzBuzz(15)[6]);
+            assertEquals(s[7], exercisesApp.fizzBuzz(15)[7]);
+            assertEquals(s[8], exercisesApp.fizzBuzz(15)[8]);
+            assertEquals(s[9], exercisesApp.fizzBuzz(15)[9]);
+            assertEquals(s[10], exercisesApp.fizzBuzz(15)[10]);
+            assertEquals(s[11], exercisesApp.fizzBuzz(15)[11]);
+            assertEquals(s[12], exercisesApp.fizzBuzz(15)[12]);
+            assertEquals(s[13], exercisesApp.fizzBuzz(15)[13]);
+            assertEquals(s[14], exercisesApp.fizzBuzz(15)[14]);
+        }
+
+    }
+    @Test
+    public void testPrimeFactors() throws Exception {
+        String s = "[2, 2, 3, 3, 5, 7, 11]";
+        assertEquals(s, exercisesApp.primeFactors(13860).toString());
+    }
+
 }
